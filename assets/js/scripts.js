fa77330bb1b88824baba2f17fb26b5f421f59596
Juan García <juan.garcia070699@outlook.es>
2018-11-21 11:36:08 -0300

    Point 1.2.2

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,3 +2,6 @@ function showText() {
   document.getElementById('hideText').style.display = 'block';
 }
 window.addEventListener("load", showText);
+function showAlert() {
+  alert("You have pressed the button");
+}
