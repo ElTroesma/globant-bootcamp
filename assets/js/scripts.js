b9aa65b431479687cb95b3b6b84451f7368d12c7
Juan García <juan.garcia070699@outlook.es>
2018-11-21 14:12:47 -0300

    Point 1.3.3

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,6 +7,6 @@ function displayAlert() {
   var req = new XMLHttpRequest();
   req.open('GET', 'http://api.icndb.com/jokes/random', false);
   req.send(null);
-  console.log(req.responseText);
+  document.getElementById('displayText').innerHTML = req.responseText;
 }
 document.getElementById('showAlert').addEventListener("click", displayAlert);
