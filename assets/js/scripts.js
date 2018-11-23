cfa8378d5bd0fad7cf36edcd95077ee203cd16aa
Juan García <juan.garcia070699@outlook.es>
2018-11-22 21:36:10 -0300

    Point 1.3.4

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,10 +3,25 @@ function showText() {
 }
 window.addEventListener("load", showText);
 
+function doRequest(method, url) {
+  var requestPromise = new Promise((resolve, reject) => {
+    var req = new XMLHttpRequest();
+    req.open(method, url, true);
+    req.send(null);
+
+    req.onreadystatechange = function () {
+      if (req.readyState == 4) {
+        resolve(req.responseText);
+      }
+    }
+  });
+
+    return requestPromise;
+}
+
 function displayAlert() {
-  var req = new XMLHttpRequest();
-  req.open('GET', 'http://api.icndb.com/jokes/random', false);
-  req.send(null);
-  document.getElementById('displayText').innerHTML = req.responseText;
+  doRequest('GET', 'http://api.icndb.com/jokes/random').then(function (responseText) {
+    document.getElementById('displayText').innerHTML = responseText;
+  })
 }
 document.getElementById('showAlert').addEventListener("click", displayAlert);
