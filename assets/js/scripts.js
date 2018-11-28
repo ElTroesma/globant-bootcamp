34bdd1caaea7dea482ffe12247d14356707c1ab7
Juan García <juan.garcia070699@outlook.es>
2018-11-28 09:41:45 -0300

    Point 1.2

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -12,3 +12,13 @@ class Movie {
 
 var matrix = new Movie("Matrix", 1999, 2.30);
 console.log(matrix);
+
+class Actor {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+var matrixActor = new Actor("Keanu Reeves", 54);
+console.log(matrixActor);
