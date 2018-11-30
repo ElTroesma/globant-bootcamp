ae2bbaefa901c30fec2db43c1e62285f560bce8e
Juan García <juan.garcia070699@outlook.es>
2018-11-30 12:21:49 -0300

    Point 2

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,17 +1,66 @@
 "use strict";
-class Movie {
+class EventEmitter {
+  constructor(eventNames) {
+    this.eventNames = eventNames;
+    this.eventCallbacks = {};
+  }
+
+  on(eventName, callback) {
+    if (!this.eventNames.includes(eventName)) {
+      throw 'El evento' + eventName + 'no existe en el objeto';
+    }
+
+    if (!this.eventCallbacks[eventName]) {
+      this.eventCallbacks[eventName] = [];
+    }
+      this.eventCallbacks[eventName].push(callback);
+  }
+
+  off(eventName, callback) {}
+
+  emit(eventName) {
+    if (!this.eventNames.includes(eventName)) {
+      throw 'El evento' + eventName + 'no existe en el objeto';
+    }
+
+    var eventHandlers = this.eventCallbacks[eventName];
+    if (eventHandlers) {
+      eventHandlers.forEach(function(callback) {
+        callback();
+      });
+    }
+  }
+}
+
+class Movie extends EventEmitter {
   constructor(title, year, duration) {
+    super(['play', 'pause', 'resume']);
     this.title = title;
     this.year = year;
     this.duration = duration;
   }
-  play() {}
-  pause() {}
-  resume() {}
+  play() {
+    this.emit('play');
+  }
+  pause() {
+    this.emit('pause');
+  }
+  resume() {
+    this.emit('resume');
+  }
 }
 
 var matrix = new Movie("Matrix", 1999, 2.30);
-console.log(matrix);
+
+matrix.on('play', function(){
+  alert('This movie`s name is ' + matrix.title + ', he trained in ' + matrix.year + '. Its duration ' + matrix.duration + ' hs.');
+});
+matrix.on('pause', function() {
+  alert('While ' + matrix.title + ' is in pause, take advantage to go to the batrrom!');
+});
+matrix.on('resume', function() {
+  alert(matrix.title + ' has been resumed. Enjoy it!')
+});
 
 class Actor {
   constructor(name, age) {
@@ -21,11 +70,3 @@ class Actor {
 }
 
 var matrixActor = new Actor("Keanu Reeves", 54);
-console.log(matrixActor);
-
-class EventEmitter {
-  constructor() {}
-  on(eventName, callback) {}
-  emit(eventName) {}
-  off(eventName, callback) {}
-}
