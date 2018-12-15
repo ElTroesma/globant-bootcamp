623e08ec0dc46b6d9cd49da348d6fbb242b28e5e
Juan García <juan.garcia070699@outlook.es>
2018-12-15 20:32:05 -0300

    Point 4

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,6 +7,9 @@ class MovieApp extends React.Component {
     this.state = { movies: [], text: '' };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.showConfirmation = this.showConfirmation.bind(this);
+    this.arrayClear = this.arrayClear.bind(this);
+    this.handleRemoveItem = this.handleRemoveItem.bind(this);
   }
 
   render() {
@@ -28,9 +31,20 @@ class MovieApp extends React.Component {
           <p className="form-tittle">These are your favorite movies!</p>
           <hr className="separation"/>
           <div id="output" className="list-container">
-            <MovieList movies={this.state.movies} />
+            <MovieList handleRemoveItem={this.handleRemoveItem} movies={this.state.movies} />
+            <button onClick={this.showConfirmation} className="submitbttn">Clear list</button>
           </div>
         </div>
+
+        <div id="hideQuestion" className="hideModal">
+          <div className="form-container">
+            <p className="form-tittle">Do you really want to clear your list?</p>
+            <hr className="separation"/>
+            <button className="submitbttn modalbtn" id="hideQuestion" onClick={this.arrayClear}>Yes</button>
+            <button className="submitbttn modalbtn" id="hideQuestion">No</button>
+          </div>
+        </div>
+
         <p className="design">Designed by Juan Garcia</p>
       </div>
 
@@ -47,6 +61,7 @@ class MovieApp extends React.Component {
       return;
     }
     const newMovie = {
+      id: this.state.movies.length,
       text: this.state.text
     };
     this.setState(state => ({
@@ -54,17 +69,50 @@ class MovieApp extends React.Component {
       text: ''
     }));
   }
+  showConfirmation() {
+    var question = document.getElementById('hideQuestion');
+
+    question.style.display = 'flex';
+    question.addEventListener('click', function() {
+      question.style.display = 'none';
+    });
+  }
+  arrayClear() {
+    this.setState(state => ({
+      movies: [],
+      text: ''
+    }));
+  }
+  handleRemoveItem(id) {
+    console.log(id);
+    const movieIndex = this.state.movies.findIndex(movie => {
+      return movie.id == id;
+    });
+    let movies = this.state.movies;
+    movies.splice(movieIndex, 1);
+    this.setState({
+      movies: movies
+    });
+  }
 }
 
 class MovieList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.removeItem = this.removeItem.bind(this);
+  }
   render() {
     return (
       <ul className="list-movie">
         {this.props.movies.map(movie => (
-          <li key={movie.id} className="list-movie-item">{movie.text}</li>
+          <li key={movie.id} className="list-movie-item" onClick={this.removeItem.bind(this, movie.id)} >{movie.text}</li>
         ))}
       </ul>
     );
+  }
+
+  removeItem(id) {
+    this.props.handleRemoveItem(id);
   }
 }
 
